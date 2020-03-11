900b226ffc7a788c44565835558ee6e7ce8a8a17
Hyukchan Kwon <kwonssy02@gmail.com>
2020-03-11 17:13:17 +0900

    chore(MockData): Sites Mock Data 추가

--- test/mockData/mockSites.js
+++ test/mockData/mockSites.js
@@ -2,6 +2,8 @@ const sites = [];
 
 insertSite(1, "현대카드", new Date(2019, 3, 26), null);
 insertSite(2, "애플", new Date(2019, 2, 28), null);
+insertSite(3, "농협은행", new Date(2018, 12, 25), null);
+insertSite(4, "삼성전자", new Date(2017, 1, 1), null);
 
 function insertSite(siteId, name, openDate, logoFileName) {
   sites.push({
